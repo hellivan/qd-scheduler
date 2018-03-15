22d7ea068f89a0ddaaf72cd10949c03c8c936367
Ivan Hell <ivan.hell@innovaalp.com>
2018-03-15 11:55:11 +0100

    style: replace tabulators by whitespaces

--- src/index.ts
+++ src/index.ts
@@ -23,23 +23,23 @@ class TaskQueue<T extends Task> {
     public size$: Observable<number>;
     
     constructor(private maxSize: number) {
-	this.size$ = this.sizeSubject.asObservable();
+        this.size$ = this.sizeSubject.asObservable();
     }
 
     get size(): number {
-	return this.tasks.length;
+        return this.tasks.length;
     }
 
     get freeSlots(): number {
-	return this.maxSize - this.size;
+        return this.maxSize - this.size;
     }
 
     get full(): boolean {
-	return this.freeSlots < 1;
+        return this.freeSlots < 1;
     }
 
     get empty(): boolean {
-	return this.size < 1;
+        return this.size < 1;
     }
     
     public push(t: T) {
@@ -49,17 +49,17 @@ class TaskQueue<T extends Task> {
     }
 
     public pop(): T {
-	if(this.empty) return undefined;
-	const t = this.tasks.shift();
-	this.sizeSubject.next(this.tasks.length);
-	return t;
+        if(this.empty) return undefined;
+        const t = this.tasks.shift();
+        this.sizeSubject.next(this.tasks.length);
+        return t;
     }
 
     public drop(t: T): boolean {
-	const index = this.tasks.indexOf(t);
-	if(index >= 0) this.tasks.splice(index, 1);
-	this.sizeSubject.next(this.tasks.length);
-	return index >= 0;
+        const index = this.tasks.indexOf(t);
+        if(index >= 0) this.tasks.splice(index, 1);
+        this.sizeSubject.next(this.tasks.length);
+        return index >= 0;
     }
 }
 
@@ -70,7 +70,7 @@ export class QdScheduler<T extends Task> {
     private taskStartingSubject = new Subject<T>();
     private taskErrorSubject = new Subject<{err: any, task: T}>();
     private taskCompletedSubject = new Subject<T>();
-    
+
     private currentTasks: TaskQueue<T>;
     private queuedTasks: TaskQueue<T>;
 
@@ -84,49 +84,49 @@ export class QdScheduler<T extends Task> {
 
     
     constructor(parallelTasks: number = 10, maxQueuedTasks: number = 200) {
-	this.currentTasks =  new TaskQueue<T>(parallelTasks);
-	this.queuedTasks = new TaskQueue<T>(maxQueuedTasks);
-	
-	this.taskQueued$ = this.taskQueuedSubject.asObservable();
-	this.taskStarting$ = this.taskStartingSubject.asObservable();
-	this.taskError$ = this.taskErrorSubject.asObservable();
-	this.taskCompleted$ = this.taskCompletedSubject.asObservable();
+        this.currentTasks =  new TaskQueue<T>(parallelTasks);
+        this.queuedTasks = new TaskQueue<T>(maxQueuedTasks);
 
-	// TODO change this?
-	this.queuedTasksCount$ = this.queuedTasks.size$;
-	this.runningTasksCount$ = this.currentTasks.size$;
+        this.taskQueued$ = this.taskQueuedSubject.asObservable();
+        this.taskStarting$ = this.taskStartingSubject.asObservable();
+        this.taskError$ = this.taskErrorSubject.asObservable();
+        this.taskCompleted$ = this.taskCompletedSubject.asObservable();
+
+        // TODO change this?
+        this.queuedTasksCount$ = this.queuedTasks.size$;
+        this.runningTasksCount$ = this.currentTasks.size$;
     }
 
     public queueTask(t: T): void {
-	this.queuedTasks.push(t);
-	this.taskQueuedSubject.next(t);
+        this.queuedTasks.push(t);
+        this.taskQueuedSubject.next(t);
     }
 
     public start(): void {
-	Observable.merge(
-	    this.taskCompletedSubject.map(x => true),
-	    this.taskQueuedSubject.map(x => true),
-	    Observable.of(true)
-	).subscribe(
-	    () => this.executeTasks()
-	);
+        Observable.merge(
+            this.taskCompletedSubject.map(x => true),
+            this.taskQueuedSubject.map(x => true),
+            Observable.of(true)
+        ).subscribe(
+            () => this.executeTasks()
+        );
     }
 
 
     private executeTasks(): void {
-	while(this.currentTasks.freeSlots > 0 && !this.queuedTasks.empty) {
-	    const task = this.queuedTasks.pop();
-	    this.currentTasks.push(task);
-	    this.taskStartingSubject.next(task);
+        while(this.currentTasks.freeSlots > 0 && !this.queuedTasks.empty) {
+            const task = this.queuedTasks.pop();
+            this.currentTasks.push(task);
+            this.taskStartingSubject.next(task);
 
-	    Promise.resolve()
-		.then(() => task.exec())
-		.catch(err => this.taskErrorSubject.next({err, task}))
-		    .then(() => {
-			this.currentTasks.drop(task);
-			this.taskCompletedSubject.next(task)
-		    });
-	}
+            Promise.resolve()
+                .then(() => task.exec())
+                .catch(err => this.taskErrorSubject.next({err, task}))
+                    .then(() => {
+                        this.currentTasks.drop(task);
+                        this.taskCompletedSubject.next(task)
+                    });
+        }
     }
 
 }
